5db472e5e296bdb42346b7a8e17792d3adc3c850
Shaikh Tauhid Shaikh Javed <tauhidshaikhsas@gmail.com>
2025-08-05 12:44:44 +0530

    made some fixes

--- dashboard/src/App.jsx
+++ dashboard/src/App.jsx
@@ -2,14 +2,14 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import './components/Home';
+import { BrowserRouter } from 'react-router-dom'
 import Home from './components/Home.jsx';
-function App() {
 
+function App() {
   return (
-    <>
+    <BrowserRouter>
       <Home/>
-    </>
+    </BrowserRouter>
   )
 }
 

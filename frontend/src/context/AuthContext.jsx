e7395addf73ba5aa02a51e8ea2af500d63daba6f
Shaikh Tauhid Shaikh Javed <tauhidshaikhsas@gmail.com>
2025-08-08 03:10:39 +0530

    .

--- frontend/src/context/AuthContext.jsx
+++ frontend/src/context/AuthContext.jsx
@@ -3,7 +3,8 @@ import axios from 'axios';
 
 // Create the context
 const AuthContext = createContext();
-const backendUrl = process.env.VITE_BACKEND_URL;
+const backendUrl = import.meta.env.VITE_BACKEND_URL;
+
 
 
 // Custom hook to use the auth context
